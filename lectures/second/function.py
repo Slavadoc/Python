8ce3b60c0384c056d4650eac707a8aaf1f1fdbe8
Slavadoc <viaceslavziablov@gmail.com>
2022-11-16 17:15:33 +0300

    lecture 3

--- lectures/second/function.py
+++ lectures/second/function.py
@@ -1,4 +1,4 @@
-def mul(_a,_b):
+def multi(_a,_b):
     return _a * _b
 
 def new_string(_symbol, _count = 3):
@@ -9,3 +9,15 @@ def concatenation(*params):
     for item in params:
         res += item
     return res
+
+def fib(_n):
+    if _n in [1,2]:
+        return 1
+    else:
+        return fib(_n-1) + fib(_n-2)
+
+def printFib(_n):
+    list = []
+    for i in range(1,_n+1):
+        list.append(fib(i))
+    return list
--- lectures/second/second.py
+++ lectures/second/second.py
@@ -1,3 +1,7 @@
+# a – открытие для добавления данных
+# r – открытие для чтения данных
+# w – открытие для записи данных
+
 # with open('data.txt', 'w') as data:
 #     data.write('line 1 \n')
 #     data.write('line 2 \n')
@@ -5,42 +9,99 @@
 # colors = ['red', 'green', 'blue']
 # data = open('data.txt', 'a')
 # data.writelines(colors)
+# data.write('\n1234')
+# data.write('\n Goll \n')
 # data.close()
 
-# path = 'data.txt'
+# path = 'neData.txt'
+# data = open(path, 'a')
+# data.write('Shtm new')
+
+# path = 'newData.txt'
 # data = open(path, 'r')
 # for line in data:
 #     print(line)
 # data.close()
 
-import function as concatenation
+# from function import *
 
 # a = int(input())
-# # b = int(input())
+# b = int(input())
 # s = [str(s) for s in input().split()]
 
-# print(function.mul(a,b))
-# print(function.new_string(s,b))
+# print(multi(a,b))
+# print(new_string(s,b))
 # print(concatenation('q', 's', 'd', 'w'))
-# # Кортежи
-# t = tuple(['red', 'green', 'blue'])
-# print(t[0]) # red
-# print(t[2]) # blue
-# # print(t[10]) # IndexError: tuple index out of range
-# print(t[-2]) # green
-# # print(t[-200]) # IndexError: tuple index out of range
-# for e in t:
-#     print(e) # red green blue
-#t[0] = 'black' # TypeError: 'tuple' object does not support
-# item assignment
+# print(printFib(a))
 
-list1 = [1,2,3,4]
+# Кортежи
+# a = (1,2,3,4)
+# print(a[-1])
+# t = (1,)
+# print(type(t))
+# print(t[0])
+# for i in range(len(a)):
+#     print(a[i])
+
+# 
+# dictionary = {}
+# dictionary = \
+#     {
+#         'up' : '↑',
+#         'down' : '↓',
+#         'left' : '←',
+#         'right' : '→'
+#     }
+# print(dictionary['left'])
+# print(dictionary['right'])
+# print(dictionary['up'])
+# print(dictionary['down'])
+
+# for key  in dictionary.keys():
+#     print(key)
+# for values in dictionary.values():
+#     print(values)
+# for item in dictionary:
+#     print(dictionary[item])
+# Вниз (↓) — Alt и 2 5.
+# Вверх (↑) — Alt и 2 4.
+# Вверх-вниз (↕) — Alt и 1 8.
+# Влево-вправо (↔) — Alt и 2 9.  
+
+#Множество 
+# colors = {'red','green','blue'}
+# print(type(colors))
+# print(colors)
+# colors.add('yellow')
+# print(colors)
+# colors.add('red')
+# print(colors)
+# b = set([2, 5, 8, 13, 21])
+
+# a = {1, 2, 3, 5, 8}
+# b = {2, 5, 8, 13, 21}
+# c = a.copy()
+# print(c)
+# u = a.union(b)
+# print(u)
+# i =  a.intersection(b)
+# print(i)
+def printList(_list):
+    for i in range(len(_list)):
+        print(_list[i], end = '')
+    print()
+
+list1 = [1, 2, 3, 4, 5]
 list2 = list1
-list1[1] = 123
-list1.pop()
+printList(list1)
+printList(list2)
+list1[0] = 321
+printList(list1)
+printList(list2)
 
-for e in list1:
-    print(e)
-print()
-for e in list2:
-    print(e)
+print(list1.pop())
+printList(list1)
+print(list1.pop())
+printList(list1)
+print(list1.pop())
+printList(list1)
