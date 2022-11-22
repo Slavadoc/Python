6a0bc73b35a3bd87842a638928581fa39452a254
Slavadoc <viaceslavziablov@gmail.com>
2022-11-22 13:12:39 +0300

    Добавлено id поиск удаление и редактирование

--- homework/hw08/controller.py
+++ homework/hw08/controller.py
@@ -4,6 +4,7 @@ from export_data import export_data
 from print_data import print_data
 from search_data import search_data
 from delete_data import delete
+from redact_data import redact
 
 
 def greeting():
@@ -45,10 +46,10 @@ def choice_todo():
         item = search_data(word, data)
         if item != None:
             print("Фамилия".center(20), "Имя".center(20),
-                  "Телефон".center(15), "Примечание".center(30))
+                "Телефон".center(15), "Примечание".center(30))
             print("-"*85)
             print(item[0].center(20), item[1].center(20),
-                  item[2].center(15), item[3].center(30))
+                item[2].center(15), item[3].center(30))
         else:
             print("Данные не обнаружены")
     elif ch == '4':
@@ -61,3 +62,9 @@ def choice_todo():
             print("Данные не обнаружены")
     elif ch == '5':
         word = input("Введите данные для поиска:  ")
+        data = export_data()
+        item = search_data(word, data)
+        if item != None:
+            redact(item[0],data)
+        else:
+            print("Данные не обнаружены")
