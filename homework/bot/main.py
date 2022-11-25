35042bf459cdb820755a7f3cf366e6a57d74cc98
Slavadoc <54536848+Slavadoc@users.noreply.github.com>
2022-11-25 19:49:44 +0300

    'hw10

--- homework/bot/main.py
+++ homework/bot/main.py
@@ -1,28 +1,30 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler
 import calc
-
+from spy import *
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+
 async def print_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message.text
+    log(update,context)
     print(msg)
     items = msg.split()
     num1 = items[1]
     num2 = items[3]
     oper = items[2]
-    result = calc.op1(num1,num2,oper)
+    result = calc.op1(num1, num2, oper)
     print(result)
     await update.message.reply_text(f'{num1} {oper} {num2} = {result}')
     # resalt = calc.calculate(num1,num2)
 
 
-app = ApplicationBuilder().token("").build()
+app = ApplicationBuilder().token("5813769900:AAFOCQ377Gu9m2h5XUuhwVywIW24Zf9a8ZQ").build()
 
 app.add_handler(CommandHandler("hello", hello))
-app.add_handler(CommandHandler('calc',print_command))
+app.add_handler(CommandHandler('calc', print_command))
 # app.add_handler(CommandHandler("min", commands.min_command))
 print('Start server')
 app.run_polling()
@@ -42,4 +44,3 @@ app.run_polling()
 #     x = int(items[1])
 #     y = int(items[2])
 #     await update.message.reply_text(f'{x} + {y} = {x+y} ')
-
