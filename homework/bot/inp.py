62fbb4ff6e8f3419716bfa04b96990534ab16d2d
Slavadoc <viaceslavziablov@gmail.com>
2022-11-28 00:42:47 +0300

    it's all

--- homework/bot/inp.py
+++
@@ -1,8 +0,0 @@
-#
-def myOutput(a):
-    print("Hello World")
-    print(a)
-    
-def myInput():
-    print("Please enter your number of the")
-    return input()
--- homework/bot/outcalc.py
+++
@@ -1,6 +0,0 @@
-#
-#import calc
-# from calc import *
-# from inp import *
-# myOutput(calculate(
-# myInput(), myInput(), myInput()))
